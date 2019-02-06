d2d3060079fd12474d17b185928447d2c951e1e5
Dima Merechko <mfocus@mail.ru>
2019-02-06 15:54:10 +0200

    add Util class for playing with ip

---
+++ src/main/java/ru/photorex/Solution.java
@@ -0,0 +1,4 @@
+package ru.photorex;
+
+public class Solution {
+}
---
+++ src/main/java/ru/photorex/Util.java
@@ -0,0 +1,52 @@
+package ru.photorex;
+
+public class Util {
+
+    public static boolean checkIp(String ip) {
+        boolean result = true;
+
+        if (!ip.contains(".")) return false;
+
+        String[] array = ip.split("\\.");
+
+        try {
+            for (String a : array)
+                Integer.parseInt(a);
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+
+        return result;
+    }
+
+    public static long ipToLong(String ip) {
+
+        String[] ipInArray = ip.split("\\.");
+
+        long result = 0;
+        for (int i = 0; i < ipInArray.length; i++) {
+
+            int power = 3 - i;
+            int temp = Integer.parseInt(ipInArray[i]);
+            result += temp * Math.pow(256, power);
+
+        }
+
+        return result;
+    }
+
+    public static String longToIp(long ip) {
+        StringBuilder sb = new StringBuilder(15);
+
+        for (int i = 0; i < 4; i++) {
+            sb.insert(0, Long.toString(ip & 0xff));
+
+            if (i < 3) {
+                sb.insert(0, '.');
+            }
+            ip = ip >> 8;
+        }
+
+        return sb.toString();
+    }
+}
